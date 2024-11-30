218d50594a48757ab49ef229b69193ffffdb60e8
Pratik <pratikkhatri520@gmail.com>
2024-11-30 23:52:31 +0530

    changes in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -20,12 +21,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-      </head>
+      <head></head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        {children}
+        <Navbar className="fixed top-0 left-0 w-full z-10" />
+        <main className="flex-1 pt-[60px] pb-[50px]">{children}</main>
+        <Footer className="fixed bottom-0 left-0 w-full z-10" />
       </body>
     </html>
   );
