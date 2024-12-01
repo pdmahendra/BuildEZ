02fe857c568753fcef18262f31af81c581fd5546
Pratik <pratikkhatri520@gmail.com>
2024-12-01 23:35:20 +0530

    fix products page

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Alata } from "next/font/google";
 import ProductCard from "../../components/product/ProductCard";
+import { ChevronRight } from "lucide-react";
+import { Alata } from "next/font/google";
+
 const alata = Alata({
   subsets: ["latin"],
   weight: "400",
@@ -8,7 +10,7 @@ const alata = Alata({
 
 const Page = () => {
   return (
-    <div>
+    <div className={`${alata.className}`}>
       <div className="grid grid-cols-1 md:grid-cols-3">
         <div className="relative col-span-2 w-full h-full hidden md:block">
           <img
@@ -79,43 +81,52 @@ const Page = () => {
           <ProductCard alata={alata} />
         </div>
       </div>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 mt-20 p-4">
-        <div
-          className={`${alata.className} flex flex-col items-center justify-center gap-y-6 md:gap-y-12`}
-        >
-          <div className={`text-3xl md:text-5xl text-start`}>
-            The Timeless Appeal of <br />
-            Natural Stone in Modern <br />
-            Design
-          </div>
-          <p className="text-xs text-start">
-            When it comes to design, the choice of building materials can
-            transform an ordinary space into an <br />
-            extraordinary one. we specialize in high-quality materials that
-            blend timeless elegance with modern <br /> aesthetics. <br /> Start
-            by clarifying your vision. Are you drawn to classic styles or
-            contemporary designs? This will guide <br /> your selections.
-            Prioritize quality and durability; materials like natural stone and
-            premium wood not <br /> only enhance beauty but also stand the test
-            of time. <br /> Explore versatile options that can be used in
-            various applications, such as decorative tiles for <br />{" "}
-            backsplashes or feature walls. Don’t forget to consider color and
-            texture—mixing smooth and rough <br /> materials can add depth and
-            interest.
-          </p>
-        </div>
+      <div className="max-w-7xl mx-auto p-4 mt-16">
         <div>
-          <img
-            src="/productImg5.jpg"
-            alt="Product Image 2"
-            className="h-full w-full object-cover"
-          />
+          <div className="grid md:grid-cols-2">
+            <div className="flex flex-col p-8 flex-grow">
+              <h3 className="text-3xl md:text-5xl mb-6">
+                The Timeless Appeal of Natural Stone in Modern Design
+              </h3>
+
+              <div className="space-y-4 max-sm:text-sm text-gray-600">
+                <p>
+                  When it comes to design, the choice of building materials can
+                  transform an ordinary space into an extraordinary one. we
+                  specialize in high-quality materials that blend timeless
+                  elegance with modern aesthetic.
+                </p>
+                <p>
+                  Start by clarifying your vision. Are you drawn to classic
+                  styles or contemporary designs? This will guide your
+                  selections. Prioritize quality and durability materials like
+                  natural stone and premium wood not only enhance beauty but
+                  also stand the test of time.
+                </p>
+                <p>
+                  Explore versatile options that can be used in various
+                  applications, such as decorative tiles for backsplashes or
+                  feature walls. Don't forget to consider color and
+                  texture—mixing smooth and rough materials can add depth and
+                  interest.
+                </p>
+              </div>
+            </div>
+
+            <div className="relative h-[400px] md:h-auto">
+              <img
+                src={"/article.png"}
+                alt="Modern interior design"
+                className="w-full h-full object-cover"
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div
-        className={`${alata.className} text-sm text-center  border-2 py-6 mt-8 tracking-widest`}
-      >
-        SEE ALL ARTICLES
+
+        <div className="mt-8 tracking-widest group flex items-center justify-center text-sm text-gray-900 hover:text-gray-700 transition-colors border-2 py-5">
+          SEE ALL ARTICLES
+          <ChevronRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform" />
+        </div>
       </div>
     </div>
   );
