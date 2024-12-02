4acb79b7f80400e4c8210b2991add2e8b53dfe5b
Pratik <pratikkhatri520@gmail.com>
2024-12-02 22:53:58 +0530

    added dynamic product detail page

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,14 +1,25 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 
-const ProductCard = ({ alata }) => {
+const ProductCard = ({ alata, image, name, price, id }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/products/${id}`);
+  };
   return (
-    <div className="h-[355px] w-[330px]">
-      <img src="/productImg4.jpg" alt="Product Image" className="w-full h-[355px] object-cover" />
+    <div className="h-[355px] w-[330px]" onClick={handleClick}>
+      <img
+        src={image}
+        alt="Product Image"
+        className="w-full h-[355px] object-cover"
+      />
       <div
         className={`${alata.className} flex justify-between text-[#323334] text-xs tracking-widest pt-2`}
       >
-        <p>IRON FRAME</p>
-        <p>$4,990</p>
+        <p>{name}</p>
+        <p>{price}</p>
       </div>
     </div>
   );
