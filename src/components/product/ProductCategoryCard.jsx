26d9f7ade3b433e954b49ad8aef8cce6fac26e43
Pratik <pratikkhatri520@gmail.com>
2024-12-02 23:26:40 +0530

    added product category card

---
+++ src/components/product/ProductCategoryCard.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+const ProductCategoryCard = ({ alata, image, name, id }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    // router.push(`/products/${id}`);
+  };
+  return (
+    <div className="h-[355px] w-[330px]" onClick={handleClick}>
+      <img
+        src={image}
+        alt="Product Image"
+        className="w-full h-[355px] object-cover"
+      />
+      <div
+        className={`${alata.className} text-center text-[#323334] text-xl pt-2`}
+      >
+        {name}
+      </div>
+    </div>
+  );
+};
+
+export default ProductCategoryCard;
