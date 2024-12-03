79df97b8efb8202e85de54454cd08af8ca94ef0e
Pratik <pratikkhatri520@gmail.com>
2024-12-03 22:02:09 +0530

    added profile card

---
+++ src/components/companyProfile/ProfileCard.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProfileCard = () => {
+  return (
+    <div>
+      <div className="w-[344px]">
+        <img src="/companyProfileImg3.jpg" alt="" className="object-cover" />
+        <div className="bg-[#EFEFEF] p-6 rounded-b-2xl space-y-2">
+          <h5 className="text-lg">H. S. Malhotra</h5>
+          <p className="text-sm">Sales Manager</p>
+          <p className="text-sm">
+            About the person response work responsiblity and etc.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
