c06a8e1f5afe49271b5344a28c9b2974f32312e2
Pratik <pratikkhatri520@gmail.com>
2024-12-09 23:38:47 +0530

    added product model

---
+++ src/app/models/productModel.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    productName: {
+      type: String,
+      required: [true, "Product name is required"],
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: [true, "Price is required"],
+      min: [0, "Price must be a positive value"],
+    },
+    productDescription: {
+      type: String,
+      required: [true, "Product description is required"],
+      trim: true,
+    },
+    // productBenefit: {
+    //   type: String,
+    //   trim: true,
+    // },
+    productDetails: {
+      type: String,
+      trim: true,
+    },
+    // primaryImage: {
+    //   type: String,
+    //   required: [true, "Primary image is required"],
+    // },
+    category: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.models.Product || mongoose.model("Product", productSchema);
+export default Product;
