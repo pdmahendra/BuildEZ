ab7760845a788aea1aaa643151bcdc8d3febe1f6
Pratik <pratikkhatri520@gmail.com>
2024-12-12 15:12:42 +0530

    added new layout

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,20 +1,20 @@
 import Sidebar from "../../components/dashboard/Sidebar";
 // import Header from "../../components/dashboard/Header";
 // import SalesChardt from "../../components/dashboard/SalesChart";
-
+import Layout from "../newLayout";
 const Dashboard = () => {
   return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        {/* <Header /> */}
-        <div className="p-6">
-          <h3 className="text-3xl font-bold mb-6">
-            Welcome to the Admin Dashboard!
-          </h3>
+    <Layout>
+      <div className="flex min-h-screen">
+        <div className="flex-1 flex flex-col">
+          {/* <Header /> */}
+          <div className="p-6">
+            <h3 className="text-3xl font-bold mb-6">
+              Welcome to the Admin Dashboard!
+            </h3>
 
-          {/* Stats Cards */}
-          {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
+            {/* Stats Cards */}
+            {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
             <div className="bg-white p-4 rounded-lg shadow-lg">
               <h4 className="text-lg font-semibold">Total Sales</h4>
               <p className="text-2xl font-bold mt-2">$24,000</p>
@@ -29,14 +29,15 @@ const Dashboard = () => {
             </div>
           </div> */}
 
-          {/* Charts Section */}
-          {/* <div className="bg-white p-6 rounded-lg shadow-lg mb-6">
+            {/* Charts Section */}
+            {/* <div className="bg-white p-6 rounded-lg shadow-lg mb-6">
             <h4 className="text-xl font-semibold mb-4">Sales Overview</h4> */}
-          {/* <SalesChart /> */}
-          {/* </div> */}
+            {/* <SalesChart /> */}
+            {/* </div> */}
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
