a4fe305e52f16748e6a18c26c755c8c45c109783
Pratik <pratikkhatri520@gmail.com>
2024-12-12 15:15:39 +0530

    added timestamps field

--- src/app/models/contactModel.js
+++ src/app/models/contactModel.js
@@ -1,32 +1,37 @@
 import mongoose, { Schema } from "mongoose";
 
-const contactSchema = new Schema({
-  fullName: {
-    type: String,
-    required: [true, "Full name is required"],
-    trim: true,
+const contactSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, "Full name is required"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: [true, "Phone Number is required"],
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+      trim: true,
+    },
+    message: {
+      type: String,
+      required: [true, "Message is required"],
+      trim: true,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    trim: true,
-  },
-  phone: {
-    type: String,
-    required: [true, "Phone Number is required"],
-    trim: true,
-  },
-  address: {
-    type: String,
-    required: [true, "Address is required"],
-    trim: true,
-  },
-  message: {
-    type: String,
-    required: [true, "Message is required"],
-    trim: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Contact = mongoose.models.Contact || mongoose.model("Contact", contactSchema);
 export default Contact;
