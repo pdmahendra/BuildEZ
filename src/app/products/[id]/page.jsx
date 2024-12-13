43b7397c930117c76e9c86c51d7b6400c8408e96
Pratik <pratikkhatri520@gmail.com>
2024-12-13 23:44:44 +0530

    integrated get product api

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import ProductImageSlider from "../../../components/productDetails/ProductImagesSlider";
 import ProductCard from "../../../components/product/ProductCard";
+import getProductById from "../../../services/getProduct";
+import getAllProducts from "../../../services/getAllProductsApi";
 import { Alata } from "next/font/google";
+import { useParams } from "next/navigation";
 
 const alata = Alata({
   subsets: ["latin"],
@@ -16,59 +20,55 @@ const productImages = [
   "/productDetailImg2.jpg",
 ];
 
-const products = [
-  {
-    id: "1",
-    image: "/productImg4.jpg",
-    name: "IRON FRAME",
-    price: "$4,990",
-  },
-  {
-    id: "2",
-    image: "/productImg4.jpg",
-    name: "IRON FRAME",
-    price: "$4,990",
-  },
-  {
-    id: "3",
-    image: "/productImg4.jpg",
-    name: "IRON FRAME",
-    price: "$4,990",
-  },
-  {
-    id: "4",
-    image: "/productImg4.jpg",
-    name: "IRON FRAME",
-    price: "$4,990",
-  },
-];
 const page = () => {
+  const { id } = useParams();
+
+  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState();
+
+  // Get products
+  const getProduct = async () => {
+    const response = await getProductById(id);
+    console.log(response.product);
+    setProduct(response.product);
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  // Get products
+  const getProducts = async () => {
+    const response = await getAllProducts();
+    console.log(response.products);
+
+    setProducts(response.products);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <div className={`${alata.className}  pb-[50px]`}>
       <div className="sm:grid sm:grid-cols-2 max-w-[90%] mx-auto mt-8">
         <div className="col-span-1">
           <div>
-            <img src="/productDetailImg1.jpg" alt="product image" />
+            <img
+              src={product?.images[0]}
+              alt="product image"
+              className="object-cover w-[600px] h-[707px] rounded-2xl"
+            />
           </div>
           <div className="sm:mr-24">
             <ProductImageSlider images={productImages} />
           </div>
         </div>
-        <div className="space-y-12 mt-8 sm:mt-4">
-          <h1 className="text-5xl text-[#000000]">Product Name</h1>
-          <div className="space-y-5">
+        <div className="space-y-12 mt-8 sm:mt-4 max-w-full">
+          <h1 className="text-5xl text-[#000000]">{product?.productName}</h1>
+          <div className="space-y-5 max-w-full">
             <h4 className="text-2xl text-[#344054]">Product Description</h4>
-            <p className="text-[#667085] text-base">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+            <p className="text-[#667085] text-base break-words">
+              {product?.productDescription}
             </p>
           </div>
           {/* <div className="space-y-5">
@@ -97,11 +97,11 @@ const page = () => {
               </p>
             </div>
           </div>{" "} */}
-          <div className="space-y-5">
+          <div className="space-y-5 max-w-full">
             {" "}
             <div className="text-2xl text-[#344054]">Product Details</div>
-            <div className="text-[#667085] space-y-4">
-              <p>
+            <div className="text-[#667085] space-y-4 break-words">
+              {/* <p>
                 It is a long established fact that a reader will be distracted
                 by the readable content
               </p>
@@ -112,9 +112,19 @@ const page = () => {
               <p>
                 It is a long established fact that a reader will be distracted
                 by the readable content
-              </p>
+              </p> */}
+              {product?.productDetails}
             </div>
           </div>{" "}
+          {product?.extraFields.length > 0 &&
+            product.extraFields.map((p, index) => (
+              <div key={index} className="space-y-5 max-w-full">
+                <div className="text-2xl text-[#344054]">{p?.heading}</div>
+                <div className="text-[#667085] space-y-4 break-words">
+                  {p?.value}
+                </div>
+              </div>
+            ))}
         </div>
       </div>
       <div className="mt-16 mb-8">
@@ -124,16 +134,16 @@ const page = () => {
           Enjoy our feature products
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-12 place-items-center">
-          {products.map((p) => (
-            <ProductCard
-              alata={alata}
-              key={p.id}
-              id={p.id}
-              image={p.image}
-              name={p.name}
-              price={p.price}
-            />
-          ))}
+          {products &&
+            products.map((p) => (
+              <ProductCard
+                alata={alata}
+                key={p._id}
+                id={p._id}
+                image={p.images[0]}
+                name={p.productName}
+              />
+            ))}
         </div>
       </div>
     </div>
