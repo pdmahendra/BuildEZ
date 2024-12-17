c5d77fd4e70951ba9d01fb3432976253c0a791f1
Pratik <pratikkhatri520@gmail.com>
2024-12-17 16:35:24 +0530

    added admin category and product card

---
+++ src/components/product/AdminCategoryCard.jsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import DeleteDialogComponent from "../DeleteDialogComponent";
+import deleteCategory from "../../services/deleteCategoryApi";
+import toast from "react-hot-toast";
+
+const AdminCategoryCard = ({ alata, image, name, id, refetch }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product-categories/${id}`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const toastId = toast.loading("Deleting Category...");
+      const response = await deleteCategory(id);
+      if (response.status === 200) {
+        refetch();
+        toast.success(response.data.message, { id: toastId });
+      }
+    } catch (error) {
+      toast.error(error.message, { id: toastId });
+    }
+  };
+
+  return (
+    <div className="h-[355px] w-[330px] cursor-pointer">
+      <img
+        src={image}
+        alt="Category Image"
+        className="w-full h-[355px] object-cover rounded-lg"
+        onClick={handleClick}
+      />
+      <div
+        className={`${alata.className} pt-2 text-center text-[#323334] text-xl flex justify-center gap-4 items-center`}
+      >
+        <p onClick={handleClick}>{name}</p>
+
+        <div>
+          <div>
+            <DeleteDialogComponent handleDelete={handleDelete} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminCategoryCard;
---
+++ src/components/product/AdminProductCard.jsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import DeleteDialogComponent from "../DeleteDialogComponent";
+import deleteProduct from "../../services/deleteProductApi";
+import { toast } from "react-hot-toast";
+
+const AdminProductCard = ({ alata, image, name, id, refetch }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/products/${id}`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const toastId = toast.loading("Deleting Product...");
+      const response = await deleteProduct(id);
+      if (response.status === 200) {
+        refetch();
+        toast.success(response.data.message, { id: toastId });
+      }
+    } catch (error) {
+      toast.error(error.message, { id: toastId });
+    }
+  };
+
+  return (
+    <div className="h-[355px] w-[330px] cursor-pointer">
+      <img
+        src={image}
+        alt="Product Image"
+        className="w-full h-[355px] object-cover rounded-lg"
+        onClick={handleClick}
+      />
+      <div
+        className={`${alata.className} text-center text-[#323334] text-xs tracking-widest pt-2 flex justify-center gap-4`}
+      >
+        <p onClick={handleClick}>{name}</p>
+
+        <div>
+          <DeleteDialogComponent handleDelete={handleDelete} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AdminProductCard;
